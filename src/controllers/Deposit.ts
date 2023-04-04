cb0feec0eb8d1d696f2493e5e4c6f9002c84d343
Minghao Chai <minghao.chai@contractors.roche.com>
2023-04-05 04:26:08 +0800

    chore: remove commented code

--- src/controllers/Deposit.ts
+++ src/controllers/Deposit.ts
@@ -5,42 +5,6 @@ import { DepositResponse } from "../models/deposit/DepositResponse";
 import { FundDeposit } from "../models/deposit/FundDeposit";
 import { Deposit } from "../services/deposit/deposit";
 
-// const depositPlans: DepositPlan[] = [
-//   {
-//     id: "DP1",
-//     type: DepositPlanType.OneTime,
-//     portfolios: [
-//       {
-//         portfolioId: "high",
-//         allocationAmount: 10000,
-//       },
-//       {
-//         portfolioId: "retire",
-//         allocationAmount: 500,
-//       },
-//     ],
-//   },
-//   {
-//     id: "DP2",
-//     type: DepositPlanType.Monthly,
-//     portfolios: [
-//       {
-//         portfolioId: "high",
-//         allocationAmount: 0,
-//       },
-//       {
-//         portfolioId: "retire",
-//         allocationAmount: 100,
-//       },
-//     ],
-//   },
-// ];
-
-// const fundDeposits: FundDeposit[] = [
-//   { amountDeposited: 10500 },
-//   { amountDeposited: 100 },
-// ];
-
 export const put = (
   req: Request<{}, {}, DepositRequest>,
   res: Response,
